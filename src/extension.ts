524fab5376194039a9b9dd9581ae790881bd1762
wk <somnuk.wk@gmail.com>
2021-02-06 15:28:34 +0700

    Test

--- src/extension.ts
+++ src/extension.ts
@@ -1,30 +1,91 @@
-'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+// this method is called when vs code is activated
 export function activate(context: vscode.ExtensionContext) {
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "pattern-decorate" is now active!');
+    console.log('decorator sample is activated');
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
+    let timeout: NodeJS.Timer | undefined = undefined;
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+    // create a decorator type that we use to decorate small numbers
+    const smallNumberDecorationType = vscode.window.createTextEditorDecorationType({
+        borderWidth: '1px',
+        borderStyle: 'solid',
+        overviewRulerColor: 'blue',
+        overviewRulerLane: vscode.OverviewRulerLane.Right,
+        light: {
+            // this color will be used in light color themes
+            borderColor: 'darkblue'
+        },
+        dark: {
+            // this color will be used in dark color themes
+            borderColor: 'lightblue'
+        }
     });
 
-    context.subscriptions.push(disposable);
-}
+    // create a decorator type that we use to decorate large numbers
+    const largeNumberDecorationType = vscode.window.createTextEditorDecorationType({
+        after: {
+            contentText: " |PRO| "
+        },
+        // cursor: 'crosshair',
+        isWholeLine: false,
+        // use a themable color. See package.json for the declaration and default values.
+        // backgroundColor: { id: 'myextension.largeNumberBackground' }
+    });
 
-// this method is called when your extension is deactivated
-export function deactivate() {
+    let activeEditor = vscode.window.activeTextEditor;
+
+    function updateDecorations() {
+        if (!activeEditor) {
+            return;
+        }
+        //const regEx = /\d+/g;
+        const regEx = /192\.168\.0\.100/g
+        const text = activeEditor.document.getText();
+        const smallNumbers: vscode.DecorationOptions[] = [];
+        const largeNumbers: vscode.DecorationOptions[] = [];
+        let match;
+        while ((match = regEx.exec(text))) {
+            const startPos = activeEditor.document.positionAt(match.index);
+            const endPos = activeEditor.document.positionAt(match.index + match[0].length);
+            const decoration = {
+                range: new vscode.Range(startPos, endPos),
+                hoverMessage: 'Number **' + match[0] + '**'
+            };
+            if (match[0].length < 3) {
+                smallNumbers.push(decoration);
+            } else {
+                largeNumbers.push(decoration);
+            }
+        }
+        activeEditor.setDecorations(smallNumberDecorationType, smallNumbers);
+        activeEditor.setDecorations(largeNumberDecorationType, largeNumbers);
+    }
+
+    function triggerUpdateDecorations() {
+        if (timeout) {
+            clearTimeout(timeout);
+            timeout = undefined;
+        }
+        timeout = setTimeout(updateDecorations, 1000);
+    }
+
+    if (activeEditor) {
+        triggerUpdateDecorations();
+    }
+
+    vscode.window.onDidChangeActiveTextEditor(editor => {
+        activeEditor = editor;
+        if (editor) {
+            triggerUpdateDecorations();
+        }
+    }, null, context.subscriptions);
+
+    vscode.workspace.onDidChangeTextDocument(event => {
+        if (activeEditor && event.document === activeEditor.document) {
+            triggerUpdateDecorations();
+        }
+    }, null, context.subscriptions);
 
 }
